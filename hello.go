2557e09075b4d417ed32d9074482362c6000fbf1
shayne gao <gaoweiwei02@gmail.com>
2025-10-30 13:31:43 +0800

    hello world!

--- hello.go
+++ hello.go
@@ -1,1 +1,41 @@
 package hello
+
+import (
+	"context"
+
+	"github.com/coredns/coredns/plugin"
+	clog "github.com/coredns/coredns/plugin/pkg/log"
+	"github.com/miekg/dns"
+)
+
+// 日志器
+var log = clog.NewWithPlugin("hello")
+
+type Hello struct {
+	Next plugin.Handler
+}
+
+func (h Hello) Name() string {
+	return "hello"
+}
+
+func (h Hello) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+
+	log.Info("Hello ServeDNS")
+
+	// 可选：如果想返回 TXT 记录作为响应，而不是只打印日志
+	// m := new(dns.Msg)
+	// m.SetReply(r)
+	// m.Authoritative = true
+	// txt := "Hello World"
+	// rr := &dns.TXT{
+	//     Hdr: dns.RR_Header{Name: r.Question[0].Name, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 60},
+	//     Txt: []string{txt},
+	// }
+	// m.Answer = append(m.Answer, rr)
+	// w.WriteMsg(m)
+	// return dns.RcodeSuccess, nil
+
+	// ???
+	return plugin.NextOrFailure(h.Name(), h.Next, ctx, w, r)
+}
--- setup.go
+++ setup.go
@@ -1,1 +1,35 @@
 package hello
+
+import (
+	"github.com/coredns/caddy"
+	"github.com/coredns/coredns/core/dnsserver"
+	"github.com/coredns/coredns/plugin"
+)
+
+// init 函数注册插件
+func init() {
+	plugin.Register("hello", setup)
+}
+
+// setup 函数解析配置
+// setup is the function that gets called when the config parser see the token "hello". Setup is responsible
+// for parsing any extra options the hello plugin may have. The first token this function sees is "hello".
+func setup(c *caddy.Controller) error {
+	c.Next() //  Ignore "hello" and give us the next token. By MZTfan 20251030 12:06
+
+	if c.NextArg() {
+		// If there was another token, return an error, because we don't have any configuration.
+		// Any errors returned from this setup function should be wrapped with plugin.Error, so we
+		// can present a slightly nicer error message to the user.
+		return plugin.Error("hello", c.ArgErr())
+	}
+
+	// Add the Plugin to CoreDNS, so Servers can use it in their plugin chain.
+	// By MZTfan 20251030 func(下一个插件) 返回(当前插件)
+	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
+		return Hello{Next: next}
+	})
+
+	// All OK, return a nil error.
+	return nil
+}
